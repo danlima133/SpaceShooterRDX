d61fd0f37f77ec71ac55294ec2686512086ad0e2
danlima133 <danielconceicaolima15@gmail.com>
2024-04-26 23:04:54 -0300

    feat: add manager componet and reagornized player sprite by level

---
+++ interfaces/Componet.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Componet
+
+export(int, 0, 1000) var _id = 0
+
+onready var currentManager:ManagerComponets = get_parent()
+
+func setId(value:int):
+	for componet in currentManager._componets:
+		if componet.getId() == value:
+			printerr("The componet can't has same ids")
+			return false
+	_id = value
+	return true
+
+func getId() -> int:
+	return _id
+
+func _init_componet():
+	pass
---
+++ manager_componets/scripts/manager_componets.gd
@@ -0,0 +1,27 @@
+extends Node
+class_name ManagerComponets
+
+signal MangerComponetsInitialize(componetsInit)
+
+var _componets:Array
+
+func _ready():
+	_componets = get_children()
+	_initialazeComponets()
+
+func getComponetsId() -> Array:
+	var ids = []
+	for componet in _componets:
+		ids.append(componet.getId())
+	return ids
+
+func getComponet(id:int):
+	for componet in _componets:
+		if id == componet.getId():
+			return componet
+	return null
+
+func _initialazeComponets():
+	for componet in _componets:
+		componet._init_componet()
+	emit_signal("MangerComponetsInitialize", _componets.size())
