6dd6c67ace4d057403ed8ab2c45672be6f0eb633
danlima133 <danielconceicaolima15@gmail.com>
2024-05-08 20:08:14 -0300

    fix: methods set and get of player flasg

--- game_objects/player/componets/scripts/player_flags.gd
+++ game_objects/player/componets/scripts/player_flags.gd
@@ -10,8 +10,11 @@ export(Dictionary) var Flags = {
 	BOUNDS: true
 }
 
-func getFlagState(flagId:int) -> bool:
-	return Flags[flagId]
+func getFlagState(flagId:int) -> int:
+	return Flags.get(flagId, ERR_DOES_NOT_EXIST)
 
-func setFlagState(flagId:int) -> bool:
-	return Flags.get(flagId, false)
+func setFlagState(flagId:int, state:bool) -> int:
+	if Flags.has(flagId):
+		Flags[flagId] = state
+		return OK
+	return ERR_DOES_NOT_EXIST
