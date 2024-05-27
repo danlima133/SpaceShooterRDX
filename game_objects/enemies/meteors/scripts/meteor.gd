7b1470e6b5488d9e58d570b5b37da6483c2721e8
danlima133 <danielconceicaolima15@gmail.com>
2024-05-27 14:00:26 -0300

    feat: test for the enemies meteor

--- game_objects/enemies/meteors/scripts/meteor.gd
+++ game_objects/enemies/meteors/scripts/meteor.gd
@@ -1,9 +1,43 @@
-extends Node2D
+extends ObjectProcess
+
+onready var hurt_box = $"../../hurt_box"
+onready var hit_box = $"../../hit_box"
+
+onready var life_time = $life_time
 
 var meteorControl:Componet
 
-func _on_hurt_box_hurtNoValue(hurtBox):
-	pass
+func _object_enter():
+	getObjectManger()._reset()
+
+func _spaw(data:Dictionary):
+	getObjetcRoot().global_position = data["position"]
+	
+	hurt_box.setActive(true)
+	hit_box.setActive(true)
+	
+	if data.has("meteor_type"):
+		meteorControl.setMeteor(data["meteor_type"])
+	
+	meteorControl.setMeteor()
+	
+	getObjetcRoot().show()
+	_setLifeTime()
+
+func _reset(data:Dictionary):
+	getObjetcRoot().hide()
+	
+	hurt_box.setActive(false)
+	hit_box.setActive(false)
+
+func _setLifeTime():
+	randomize()
+	
+	life_time.wait_time = rand_range(4, 7)
+	life_time.start()
 
 func _on_manager_componets_MangerComponetsInitialize(componetsInit, manager:ManagerComponets):
 	meteorControl = manager.getComponet(45)
+
+func _on_life_time_timeout():
+	getObjectManger()._reset()
