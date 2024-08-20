e8288caa1d3cd876ff8a73017450bb84014fa27f
danlima133 <danielconceicaolima15@gmail.com>
2024-08-20 01:14:00 -0300

    feat: improvements and tests Rects Interactions

--- libs/regular_math.lib.gd
+++ libs/regular_math.lib.gd
@@ -1,36 +1,117 @@
 extends Script
 
-static func getSide(rectBase:Rect2, rectTarget:Rect2, offset:int = 0):
-	var rect:Rect2
-	if rectBase.position == rectTarget.position:
-		return null
-	if rectBase.position.x < rectTarget.position.x:
-		rect.position.x = rectTarget.position.x - rectBase.size.x
-	elif rectBase.position.x > rectTarget.position.x:
-		rect.position.x = rectTarget.position.x + rectTarget.size.x
-	
-	if rectBase.position.y < rectTarget.position.y:
-		rect.position.y = rectTarget.position.y - rectBase.size.y
-	elif rectBase.position.y > rectTarget.position.y:
-		rect.position.y = rectTarget.position.y + rectTarget.size.y
-	rect.size = rectBase.size
-	return rect
+class RectsMap:
+	var _config:Dictionary
+	var _map:Map
 
-static func getRectNoIntersects(rects:Array, testRect:Rect2):
-	var output:Rect2
-	var dir:Vector2
-	var lastRect:Rect2
-	if not rects.size() > 0:
-		output = testRect
-	for rect in rects:
-		if testRect.intersects(rect):
-			print(testRect)
-			var newR = getSide(testRect, rect)
-			if newR != null:
-				testRect = newR
-				output = newR
-				print(testRect, rect)
-				break
+	class Map:
+		var _rectsMap:RectsMap
+		var _data:Dictionary
+		
+		func _init(data:Dictionary = {}, rectsMap:RectsMap = null):
+			_data = data
+			_rectsMap = rectsMap
+		
+		func getAllRectsOnMap():
+			return _data.values()
+
+		func getRectByPosition(position:Vector2):
+			return _data.get(position, null)
+		
+		func getRectsInvalids(rectsGroup:Array):
+			var rectsInvalids:Array = []
+			for rectToValid in _data.values():
+				for rectInGroup in rectsGroup:
+					if rectToValid.intersects(rectInGroup):
+						rectsInvalids.append(rectToValid)
+		
+		func setRectsMap(rectsMap:RectsMap):
+			_rectsMap = rectsMap
+
+		func removeRect(position:Vector2):
+			return _data.erase(position)
+		
+		func addRect(rect:Rect2, parseRects:bool = true):
+			if parseRects:
+				rect = _rectsMap.parseRects(rect)
+			var position = rect.position
+			if not _data.has(position):
+				_data[position] = rect
+				return OK
+			return ERR_ALREADY_EXISTS
+		
+		func clearMap():
+			_data.clear()
+	
+	func _init(map:Map = null, config:Dictionary = {}):
+		self._config = config
+
+		if map == null:
+			_map = Map.new({}, self)
 		else:
-			output = testRect
-	return output
+			_map = map
+			_map.setRectsMap(self)
+
+	func _getSideByRectEquals(rectOrigin:Rect2, rectTarget:Rect2):
+		var directionsAnvaliable = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
+		var newRect:Rect2
+		for direction in directionsAnvaliable:
+			if rectOrigin.intersects(rectTarget):
+				newRect = _getSideByDirection(rectOrigin, rectTarget, direction, false)
+				if not newRect.intersects(rectTarget):
+					break
+		return newRect
+
+	func _getSideByDirection(rectOrigin:Rect2, rectTarget:Rect2, direction:Vector2 = Vector2.ZERO, equals:bool = true):
+		if equals:
+			if rectOrigin == rectTarget:
+				return _getSideByRectEquals(rectOrigin, rectTarget)
+		
+		var newRect:Rect2 = Rect2(0, 0, 0, 0)
+		var dir = direction
+
+		if dir == Vector2.ZERO:
+			dir = rectTarget.position
+		
+		if rectOrigin.position.x < dir.x:
+			newRect.position.x = rectTarget.position.x - rectOrigin.size.x
+		elif rectOrigin.position.x > dir.x:
+			newRect.position.x = rectTarget.position.x + rectTarget.size.x
+			
+		if rectOrigin.position.y < dir.y:
+			newRect.position.y = rectTarget.position.y - rectOrigin.size.y
+		elif rectOrigin.position.y > dir.y:
+			newRect.position.y = rectTarget.position.y + rectTarget.size.y
+		
+		newRect.size = rectOrigin.size
+		
+		return newRect
+
+	func _getConfig() -> Dictionary:
+		return _config
+	
+	func parseRects(rectToParse:Rect2, newRectEnterMap:bool = true):
+		var newRect:Rect2
+		var rectsGroup = _map.getAllRectsOnMap()
+		
+		if not rectsGroup.size() > 0:
+			newRect = rectToParse
+		
+		for rect in rectsGroup:
+			if rectToParse.intersects(rect):
+				var refDirection:Vector2 = Vector2.ZERO
+				if _getConfig().has("axisFix"):
+					var axisiFix = _getConfig()["axisFix"]
+					
+					if !axisiFix:
+						refDirection = rect.get_center()
+
+					newRect = _getSideByDirection(rectToParse, rect,  refDirection)
+					rectToParse = newRect
+			else:
+				newRect = rectToParse
+		
+		if newRectEnterMap:
+			_map.addRect(newRect, false)
+					
+		return newRect
--- tests/game/scripts/test_no_rects_intersects.gd
+++ tests/game/scripts/test_no_rects_intersects.gd
@@ -2,41 +2,21 @@ extends Node2D
 
 const RegularMath = preload("res://libs/regular_math.lib.gd")
 
-var rectsWolrd = []
-
-var rectsTest = [
-	Rect2(10, 30, 50, 80),
-	Rect2(-20, 60, 20, 50),
-	Rect2(45, 90, 70, 35),
-	Rect2(140, 20, 50, 20),
-	Rect2(-40, 25, 30, 30),
-	Rect2(34, 30, 45, 40)
-]
+var rects:Array
 
 func _draw():
-	for rectW in rectsWolrd:
-		draw_rect(rectW, Color.red, false)
-		draw_circle(rectW.get_center(), 2, Color.white)
-	
-	draw_circle(global_position, 10, Color.purple)
+	for rect in rects:
+		draw_rect(rect, Color.red, false)
+		draw_circle(rect.get_center(), 5, Color.white)
 
 func _ready():
-#	var rectA = Rect2(376, 0, 30, 30)
-#	var rectB = Rect2(377, 0, 30, 30)
-#	var rectTest = RegularMath.getSide(rectA, rectB)
-#	if rectTest != null:
-#		rectsWolrd.append(rectTest)
-#	rectsWolrd.append(rectB)
-#	print(rectTest)
-	for value in range(5):
-		randomize()
-		var x = value + int(rand_range(100, 200))
-		var y = value + int(rand_range(100, 200))
-		rectsTest.append(Rect2(x, y, (randi() % 50 + 10), (randi() % 50 + 10)))
-#
-	for rectT in rectsTest:
-		var rect = RegularMath.getRectNoIntersects(rectsWolrd, rectT)
-		rectsWolrd.append(rect)
+	var map = RegularMath.RectsMap.Map.new()
+	var mapRects = RegularMath.RectsMap.new(map, {
+		"axisFix": true
+	})
+
+	map.addRect(Rect2(10, 25, 85, 40))
+	map.addRect(Rect2(6, 10, 40, 45))
+
+	rects = map.getAllRectsOnMap()
 	update()
-	print(rectsWolrd)
-	
