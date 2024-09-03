dea15ce6207a8949574559b82a6f6346f7584f38
danlima133 <danielconceicaolima15@gmail.com>
2024-09-03 18:53:10 -0300

    feat: improvements and tests

--- tests/game/scripts/test_map_positions.gd
+++ tests/game/scripts/test_map_positions.gd
@@ -12,20 +12,19 @@ const textures = [
 func _ready():
 	var map = RegularMath.RectsMap.Map.new({
 		"size": {
-			"x": 9,
-			"y": 5
+			"x": 10,
+			"y": 1
 		},
 		"cellSize": 125,
-		"origin": global_position,
+		"origin": Vector2(10.24, 0),
 		"debug": true
 	})
 	add_child(map)
-	map.redefineOrigin(Vector2(562-9*125, 312-9*125))
 
 	var algorithm = RandomNumberGenerator.new()
 	algorithm.randomize()
 	var controller = RegularMath.RectsMap.new(map, algorithm)
-	var positions = controller.getPositionsByCount(7)
+	var positions = controller.getPositionsByCount(2)
 	for pos in positions:
 		var sprite = Sprite.new()
 		var texture = textures[algorithm.randi() % textures.size()]
